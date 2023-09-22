645de0cdf0bcbe4b98b507935835f922c5262ef6
Yaonemi Cruz <cruzt.nemi@gmail.com>
2023-09-22 00:28:54 -0600

    clase02 completada

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Header from "./componentes/Header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Patatas <code>fritas</code> fritas 🤔
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprende React
-        </a>
-      </header>
+    <div>
+      <Header />
     </div>
   );
 }
---
+++ src/componentes/Header/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="/img/header.png" alt="Org's Header" />
+    </header>
+  );
+};
+
+export default Header;
